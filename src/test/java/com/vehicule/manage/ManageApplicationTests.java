0c49ed576557ad769feba6b6ae72d0e8c00bdf66
farti-azzeddine <60224909+farti-azzeddine@users.noreply.github.com>
2020-06-14 00:21:33 +0100

    firstcommit

---
+++ src/test/java/com/vehicule/manage/ManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicule.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
