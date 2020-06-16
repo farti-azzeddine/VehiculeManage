2fa91dab2c9a8f527d500adff5f0cc8bda7db58e
farti-azzeddine <60224909+farti-azzeddine@users.noreply.github.com>
2020-06-16 02:08:36 +0100

    Show the list of country and add new country to DB

--- src/main/java/com/vehicule/manage/controllers/CountryController.java
+++ src/main/java/com/vehicule/manage/controllers/CountryController.java
@@ -1,14 +1,31 @@
 package com.vehicule.manage.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.vehicule.manage.models.Country;
+import com.vehicule.manage.services.CountryService;
 
 @Controller
 public class CountryController {
+
+	@Autowired CountryService countryService;
 	
 	@GetMapping("/countries")
-	public String GetCountries() {
+	public String GetCountries(Model model) {
+		List<Country> countryList = countryService.getCountries();
+		model.addAttribute("countries", countryList);
 		return "Country";
 	}
-
+	
+	@PostMapping("countries/addNew")
+    public String addNewCountries(Country country) {
+	countryService.save(country);
+	return "redirect:/countries";
+     }
 }
--- src/main/java/com/vehicule/manage/services/CountryService.java
+++ src/main/java/com/vehicule/manage/services/CountryService.java
@@ -1,8 +1,28 @@
 package com.vehicule.manage.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.vehicule.manage.models.Country;
+import com.vehicule.manage.repositories.CountryRepository;
 
 @Service
 public class CountryService {
+	
+	@Autowired
+	private CountryRepository countryRepository;
+	
+    //	return list of countries 
+	public List<Country> getCountries(){
+		return countryRepository.findAll();
+	}
+	
+//	save a new country
+	public void save(Country country) {
+	
+		countryRepository.save(country);
+	}
 
 }
